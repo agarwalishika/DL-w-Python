04037b6d8f11052afad7cb73b776300ebf5754eb
Ishika <agarwali@purdue.edu>
2022-08-08 10:51:04 -0700

    educational comments

--- mnist_cnn.py
+++ mnist_cnn.py
@@ -35,10 +35,16 @@ matrix and it leaves a 28x28x1 picture into a 26x26x1 picture (32 filter are app
 leaves us with an output of 26x26x632). This filter usually moves along the picture column by 
 column (stride = 1), but you can change that so it moves every 2 columns (stride = 2). This will
 donwsample your image further.
-
-
 '''
 model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
+
+'''
+What is this MaxPooling2D layer? The previous layer outputs a 26x26 map, but this layer returns a
+13x13 map. Max pooling allows you to downsample your feature map (kinda like the strides). Max pooling
+works like a filter, except it takes the max value in the window instead of doing any matrix
+multiplication. Pooling helps you reduce the number of parameters so you don't overfit. You can
+also do an average pooling instead of a max pooling.
+'''
 model.add(layers.MaxPooling2D((2, 2)))
 model.add(layers.Conv2D(64, (3, 3), activation='relu'))
 model.add(layers.MaxPooling2D((2, 2)))
