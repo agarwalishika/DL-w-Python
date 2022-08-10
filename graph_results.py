491cf8b92a0a741882d44a9d9e984dc334325c43
Ishika <agarwali@purdue.edu>
2022-08-09 18:43:34 -0700

    graphing as an importable function

---
+++ graph_results.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+
+def eval_model(history):
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+
+    epochs = range(1, len(acc) + 1)
+
+    plt.plot(epochs, acc, 'bo', label = 'Training accuracy')
+    plt.plot(epochs, val_acc, 'b', label = 'Validation accuracy')
+    plt.legend()
+    plt.figure()
+    plt.plot(epochs, loss, 'bo', label = 'Training loss')
+    plt.plot(epochs, val_loss, 'b', label = 'Validation loss')
+    plt.legend()
+    plt.show()
